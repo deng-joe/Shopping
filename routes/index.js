c537bb440d22d2c957ca9477ea702c0a74386f11
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-02-13 16:26:59 +0300

    Make cart views

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,38 @@ var Cart = require('../models/cart');
 var Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Product.find(function (err, docs) {
-    var productChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i, i + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping Application', products: productChunks });
-  });
+router.get('/', function (req, res, next) {
+    Product.find(function (err, docs) {
+        var productChunks = [];
+        var chunkSize = 3;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', {title: 'Shopping Application', products: productChunks});
+    });
 });
 
 router.get('/add-to-cart/:id', function (req, res, next) {
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
 
-  Product.findById(productId, function (err, product) {
-    if (err) {
-      return res.redirect('/');
+    Product.findById(productId, function (err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    });
+});
+
+router.get('/shoppingCart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shoppingCart', {products: null});
     }
-    cart.add(product, product.id);
-    req.session.cart = cart;
-    console.log(req.session.cart);
-    res.redirect('/');
-  });
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shoppingCart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
